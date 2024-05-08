fb320f448325706335491a8fcd8a6c1bb4b27919
Gabriel Carvalho <gabrielcarvalhogrd@gmail.com>
2024-05-07 21:38:29 -0300

    Instalando o electron reload

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 const { app, BrowserWindow } = require('electron')
 
+const path = require('path')
+
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+});
+
+
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 800,
